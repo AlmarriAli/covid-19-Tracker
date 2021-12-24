95f18ea729c467a3d05b6ef07f87da2a3c86527d
Ali Almarri <100429371@unimail.derby.ac.uk>
2021-12-24 18:35:34 +0000

    feat: refactor home page to use maps

--- src/components/countries/CountriesList.tsx
+++ src/components/countries/CountriesList.tsx
@@ -1,7 +1,23 @@
-const CountriesList = () => {
-    return (
-        <> <h3>Countries List</h3> </>
+import { ReactElement, useContext } from "react"
+import { CountriesContext } from "../../contexts/countriesContext"
+import { ICountrydata } from "../../interfaces/CountryData"
+import CountrySummary from "./CountrySummary"
+import { Grid } from "@mui/material";
 
+
+const CountriesList = (): ReactElement => {
+    const countriesData: ICountrydata = useContext(CountriesContext)
+
+    return (
+        <> <h3>Countries List</h3>
+            <Grid container justifyContent="center" direction="row">
+
+                {!countriesData["data"] ? "...Getting Data" :
+
+                    countriesData.data.map(country => <CountrySummary />)
+                }
+            </Grid>
+        </>
     )
 
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,33 @@
 import WorldDataSummary from "../components/world/WorldDataSummary"
-
+import { Grid } from "@mui/material";
+import MapWorldComponent from "../components/maps/MapWorld";
+import WorldPieChart from "../components/charts/WorldPieChart";
+import WorldMultiBarChart from "../components/charts/WorldMultiBarChart";
 const HomePage = () => {
     return (<>
         <h2> Home page</h2>
-        <WorldDataSummary></WorldDataSummary>
+        <Grid container justifyContent="center" direction="row">
+            <Grid item md={3} sm={12} xs={12} className="border">
+                <WorldDataSummary></WorldDataSummary>
+            </Grid>
+            <Grid item md={9} sm={12} xs={12} className="border">
+                <Grid container justifyContent="center" direction="row">
+                    <Grid item md={6} sm={12} xs={12} className="border">
+                        <MapWorldComponent></MapWorldComponent>
+                    </Grid>
+                    <Grid item md={3} sm={6} xs={12} className="border">
+                        <WorldPieChart></WorldPieChart>
+                    </Grid>
+                    <Grid item md={3} sm={6} xs={12} className="border">
+                        <WorldMultiBarChart />
+                        <WorldMultiBarChart />
+                        <WorldMultiBarChart />
+                    </Grid>
+                </Grid>
+            </Grid>
+
+
+        </Grid>
     </>
 
 
