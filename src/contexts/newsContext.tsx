63f1b3f389046c247b0fb1c437b84174e0978bf8
AliAlmari <100429371@unimail.derby.ac.uk>
2022-02-26 23:37:39 +0000

    feat: retrieve news dynamically

--- src/contexts/newsContext.tsx
+++ src/contexts/newsContext.tsx
@@ -9,8 +9,8 @@ export const NewsContext = createContext<any>({});
 
 
 const NewsProvider = ({ children }: any) => {
-    const [headLines, setHeadLines] = useState<any>(mockHeadlines);
-    const [allNews, setAllnews] = useState<any>(mockAllNews)
+    const [headLines, setHeadLines] = useState<any>();
+    const [allNews, setAllnews] = useState<any>()
     const [isLoading, setIsLoading] = useState(false)
 
     const apiKey = "33e9ea497d434bdfaad27c6a1c34bd32"
@@ -45,8 +45,8 @@ const NewsProvider = ({ children }: any) => {
 
 
     useEffect(() => {
-        // getHeadlines();
-        // getAllNews();
+        getHeadlines();
+        getAllNews();
     }, [])
 
     console.log('headLines :>> ', headLines);
